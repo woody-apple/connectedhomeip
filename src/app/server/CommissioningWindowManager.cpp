799becdceb36bd80c1a58dcdfdb8e2b3b8909cee
Justin Wood <woody@apple.com>
2023-08-22 15:55:36 -0700

    Revert "[Telink] Block device role changing into Router if commissioning window opened and device not yet Router (#25395)"
    This reverts commit c1261582574ea53ecb6a261cc63e9b2aab8916a9.

--- src/app/server/CommissioningWindowManager.cpp
+++ src/app/server/CommissioningWindowManager.cpp
@@ -25,10 +25,6 @@
 #include <platform/CommissionableDataProvider.h>
 #include <platform/DeviceControlServer.h>
 
-#if CHIP_DEVICE_CONFIG_ENABLE_THREAD && CHIP_DEVICE_CONFIG_THREAD_FTD
-using namespace chip::DeviceLayer;
-#endif
-
 using namespace chip::app::Clusters;
 using namespace chip::System::Clock;
 
@@ -111,15 +107,6 @@ void CommissioningWindowManager::ResetState()
 #endif
 
     UpdateWindowStatus(CommissioningWindowStatusEnum::kWindowNotOpen);
-
-#if CHIP_DEVICE_CONFIG_ENABLE_THREAD && CHIP_DEVICE_CONFIG_THREAD_FTD
-    // Recover Router device role.
-    if (mRecoverRouterDeviceRole)
-    {
-        ThreadStackMgr().SetRouterPromotion(true);
-        mRecoverRouterDeviceRole = false;
-    }
-#endif
 
     UpdateOpenerFabricIndex(NullNullable);
     UpdateOpenerVendorId(NullNullable);
